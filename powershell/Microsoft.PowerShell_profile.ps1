66aa06265e6c551538d5c90b9054eabb95ebf7a6
Benedek <szbene.github@gmail.com>
2024-05-04 14:47:45 +0200

    update pwsh profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,8 @@
 # SET COMMAND ALIASES AND STUFF
 
 # reload powershell profile (by reloading powershell)
-function psreload { pwsh }
-#function reload { . $PROFILE } # this doesnt work for some reason
+function psreload { pwsh } # this relaunches powershell
+#function psreload { & $PROFILE }
 
 # edit this profile with micro
 function psedit { micro $PROFILE }
@@ -14,6 +14,9 @@ Set-Alias -Name clear -Value Clear-Host
 # set history command to show full history
 function Show-History { Get-Content (Get-PSReadLineOption).HistorySavePath }
 Set-Alias -Name history -Value Show-History
+
+# cheatsheet
+Set-Alias -Name cheat -Value C:\Users\szBene\cheat.exe
 
 # find executable/script path
 function which($name) {
@@ -48,75 +51,113 @@ Set-Alias -Name sysscanrepair -Value SystemScanAndRepair
 Set-Alias -Name syssr -Value SystemScanAndRepair
 
 # get public ipv4 address
-function getpubip {
-	echo "Public IPv4 address:"
+function ip {
+	Write-Output "Public IPv4 address:"
 	(Invoke-WebRequest https://ifconfig.me/ip).Content
 }
 
 # find a file in current directory and subdirectories
 function find($file, $mode) {
 	Get-ChildItem -recurse -filter "*${file}*" -ErrorAction SilentlyContinue | ForEach-Object {
-	if ($mode -ieq "d"){
-		# detailed version with more info
-		$_
-	} else {
-		# normal version, only filepaths
-		# if printed as string, only outputs full path, not all info
-		"$_"
+		if ($mode -ieq "d") {
+			# detailed version with more info
+			$_
+		}
+		else {
+			# normal version, only filepaths
+			# if printed as string, only outputs full path, not all info
+			"$_"
 		}
 	}
 }
 
-function proc($action, $name){
-	if ($action -ieq "get"){
+function proc($action, $name) {
+	if ($action -ieq "get") {
 		# get process by name
 		Get-Process $name
-	} elseif ($action -ieq "kill"){
+	}
+ elseif ($action -ieq "kill") {
 		# kill process by process name
 		Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
-	} else {
-		Write-Host "invalid parameter for action: $action"
-		Write-Host "{get, kill}"
+	}
+ else {
+		Write-Output "invalid parameter for action: $action"
+		Write-Output "{get, kill}"
 	}
 }
 
 # create empty file
-function mkfile($fn){
+function mkfile($fn) {
 	"" >> $fn  # if the file exists, appends an empty string to its content
 }
 Set-Alias -Name touch -Value mkfile
 
-function showwifi($name){
+function showwifi($name) {
 	netsh wlan show profile name=$name key=clear
 }
 
 # optimize video without changing framerate and resolution
-function optvid ( $video, $outname, $quality = 'high' ){
+function optvid ( $video, $outname, $quality = 'high' ) {
 
 	$crf = switch ($quality) {
 		'ultrahigh'	{ 0 }
 		'extrahigh'	{ 10 }
-		'high'		{ 20 }
-		'mid'  		{ 28 }
-		'low'  		{ 32 }
-		'extralow'  { 36 }
+		'high' { 20 }
+		'mid' { 28 }
+		'low' { 32 }
+		'extralow' { 36 }
 	}
 	
-	echo "Optimizing $video to $quality ($crf)"
-	echo "The video will be saved as $outname"
+	Write-Output "Optimizing $video to $quality ($crf)"
+	Write-Output "The video will be saved as $outname"
 	
 	ffmpeg -i $video -vcodec libx265 -crf $crf "$outname"
 }
+
+function unziptd($file, $outputDirectory = ".") {
+	# Extract filename without extension
+	$fileName = [System.IO.Path]::GetFileNameWithoutExtension($file)
+  
+	# Check if output directory is a valid path
+	if (Test-Path $outputDirectory) {
+		$destinationPath = Join-Path $outputDirectory -ChildPath $fileName
+	}
+ else {
+		# Handle invalid output directory (e.g., throw error or use default)
+		Write-Error "Invalid output directory path: $outputDirectory"
+		return
+	}
+  
+	# Create output directory if it doesn't exist
+	if (!(Test-Path $destinationPath)) {
+		New-Item -ItemType Directory -Path $destinationPath
+	}
+  
+	# Write output message
+	Write-Output "Extracting $file to $destinationPath"
+  
+	# Expand archive to the created directory
+	Expand-Archive -Path $file -DestinationPath $destinationPath
+}
+
+function grep($regex, $dir) {
+	if ( $dir ) {
+		Get-ChildItem $dir | select-string $regex
+		return
+	}
+	$input | select-string $regex
+}
+  
 #--------------------
 # INIT STUFF
 
 # Print "Administrator" if the instance is elevated
-if ( ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") ){
-	"Administrator"
+if ( ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") ) {
+	Write-Output "Administrator"
 }
 
-# to be fancy
-neofetch
+# fastfetch, just to be fancy
+C:\Users\szBene\.fastfetch\fastfetch.exe -c C:\Users\szBene\.fastfetch\config.jsonc
 
 # load oh my posh 
 oh-my-posh init pwsh --config "$env:USERPROFILE\clean-detailed.omp.json" | Invoke-Expression
@@ -133,3 +174,16 @@ Set-PSReadLineOption -PredictionSource History
 # Set the UpArrow and DownArrow keys to search the command history
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Enhanced PowerShell Experience
+Set-PSReadLineOption -Colors @{
+	Command   = 'Cyan'
+	Parameter = 'White'
+	String    = 'Yellow'
+	Number    = 'Magenta'
+	Variable  = 'Green'
+	Operator  = 'Gray'
+	Comment   = 'DarkGreen'
+	Keyword   = 'DarkCyan'
+	Type      = 'DarkYellow'
+}
